30cc605f5767b91cef704587eb5ea9ae4b708d83
HuyNgocDuong <ngochuyduong288@gmail.com>
2023-10-15 08:21:50 +0700

    stuff

---
+++ processEOI.php
@@ -0,0 +1,30 @@
+<?php
+$Firstname = $_POST['Firstname'];
+$Lastname = $_POST['Lastname'];
+$Dob = $_POST['Dob'];
+$Gender = $_POST['Gender'];
+$Email = $_POST['Email'];
+$Phone = $_POST['Phone'];
+$Address = $_POST['Address'];
+$Suburb = $_POST['Suburb'];
+$State = $_POST['State'];
+$Postcode = $_POST['Postcode'];
+$Job_prefer = $_POST['Job_prefer'];
+$Job_reference_number = $_POST['Job_reference_number'];
+$Programming = $_POST['Programming_Language'];
+$Skills = $_POST['Skills'];
+
+// Database connection
+$conn = new mysqli('localhost', 'root', '2882005', 's104471956_db');
+
+if ($conn->connect_error) {
+    die('Connection Failed: ' . $conn->connect_error);
+} else {
+    $stmt = $conn->prepare("INSERT INTO ApplyForm_Assign2 (Firstname, Lastname, Dob, Gender, Email, Phone, Address, Suburb, State, Postcode, Job_prefer, Job_reference_number, Programming, Skills) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssssssssss", $Firstname, $Lastname, $Dob, $Gender, $Email, $Phone, $Address, $Suburb, $State, $Postcode, $Job_prefer, $Job_reference_number, $Programming, $Skills);
+    $stmt->execute();
+    echo "Registration Successful.";
+    $stmt->close();
+    $conn->close();
+}
+?>
