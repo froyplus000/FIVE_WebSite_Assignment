a0fca1474e09543b3f5faf7952036a0ef7e73d20
HuyNgocDuong <ngochuyduong288@gmail.com>
2023-10-16 16:46:59 +0700

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,4 +3,10 @@
     $user = "s104471956";
     $pwd = "2882005";
     $sql_db = "s104471956_db";
+
+    $con = mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if(!$con){
+        die("Connection Error");
+    }
 ?>
