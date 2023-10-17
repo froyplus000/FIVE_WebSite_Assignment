a8da3c51dcf980c900a0dbb653a4ddb05e74784d
HuyNgocDuong <ngochuyduong288@gmail.com>
2023-10-17 20:00:21 +0700

    stuff

--- processEOI.php
+++ processEOI.php
@@ -96,4 +96,64 @@ $result = mysqli_query($con,$query);
     </div>
 </body>
 </html>  -->
+<style>
+    .mycss{
+    color: green;
+    border:1px solid #000;
+    padding: 10px;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    }
+    *{
+    margin: 0;
+    padding: 0;
+    background-color:#e6dcce;
+}
+</style>
+<?php
+require_once('settings.php');
+//Insert name//
+$Firstname = $_POST['Firstname'];
+$Lastname = $_POST['Lastname'];
+$Dob = $_POST['Dob'];
+$Gender = $_POST['Gender'];
+$Email = $_POST['Email'];
+$Phone = $_POST['Phone'];
+$Address = $_POST['Address'];
+$Suburb = $_POST['Suburb'];
+$State = $_POST['State'];
+$Postcode = $_POST['Postcode'];
+$Job_prefer = $_POST['Job_prefer'];
+$Job_reference_number = $_POST['Job_reference_number'];
 
+// Convert arrays to strings
+// if (is_array($_POST['Programming_Language']) && !empty($_POST['Programming_Language'])) {
+//     $Programming_language = implode(",", $_POST['Programming_Language']);
+// } else {
+//     $Programming_language = ""; 
+// }
+
+// if (is_array($_POST['Skills']) && !empty($_POST['Skills'])) {
+//     $Skills = implode(",", $_POST['Skills']);
+// } else {
+//     $Skills = "";  
+// }
+//connection//
+$con = mysqli_connect($host, $user, $pwd, $sql_db);
+
+if(mysqli_connect_errno()){
+    echo "$con->connect_error";
+    die("Connection Failed : ". mysqli_connect_error());
+} 
+    $sql="insert into ApplyForm_Assign2(Firstname, Lastname, Dob, Gender, Email, Phone, Address, Suburb, State, Postcode, Job_prefer, Job_reference_number, Programming_Language, Skills) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt = mysqli_stmt_init($con);
+    if (!mysqli_stmt_prepare($stmt,$sql)){
+        die(mysqli_error($con));
+    }   
+    mysqli_stmt_bind_param($stmt, "ssssssssssssss", $Firstname, $Lastname, $Dob, $Gender, $Email, $Phone, $Address, $Suburb, $State, $Postcode, $Job_prefer, $Job_reference_number, $Programming_language, $Skills);
+    mysqli_stmt_execute($stmt);
+echo "<p class='mycss'> Sending successfully.</p>";
+?>
--- settings.php
+++ settings.php
@@ -3,35 +3,4 @@
     $user = "s104471956";
     $pwd = "2882005";
     $sql_db = "s104471956_db";
-
-    //Insert name//
-    $Firstname = $_POST['Firstname'];
-    $Lastname = $_POST['Lastname'];
-    $Dob = $_POST['Dob'];
-    $Gender = $_POST['Gender'];
-    $Email = $_POST['Email'];
-    $Phone = $_POST['Phone'];
-    $Address = $_POST['Address'];
-    $Suburb = $_POST['Suburb'];
-    $State = $_POST['State'];
-    $Postcode = $_POST['Postcode'];
-    $Job_prefer = $_POST['Job_prefer'];
-    $Job_reference_number = $_POST['Job_reference_number'];
-    $Programming_language = $_POST['Programming_Language'];
-    $Skills = $_POST['Skills'];
-    //connection//
-    $con = mysqli_connect($host, $user, $pwd, $sql_db);
-
-    if(mysqli_connect_errno()){
-		echo "$con->connect_error";
-		die("Connection Failed : ". mysqli_connect_error());
-	} 
-        $sql=("insert into ApplyForm_Assign2(Firstname, Lastname, Dob, Gender, Email, Phone, Address, Suburb, State, Postcode, Job_prefer, Job_reference_number, Programming_Language, Skills) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt = mysqli_stmt_init($con);
-if (!mysqli_stmt_prepare($stmt,$sql)){
-    die(mysqli_error($con));
-}   
-mysqli_stmt_bind_param($stmt, "ssssssssssssss", $Firstname, $Lastname, $Dob, $Gender, $Email, $Phone, $Address, $Suburb, $State, $Postcode, $Job_prefer, $Job_reference_number, $Programming_language, $Skills);
-
-mysqli_stmt_execute($stmt);
 ?>
