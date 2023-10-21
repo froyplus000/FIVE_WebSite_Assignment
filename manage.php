864eaaca4745aed51182d20ad4167e89a5ecde36
froyplus000 <106987733+froyplus000@users.noreply.github.com>
2023-10-21 12:58:46 +1100

    PHP Update
    Added:
    
    - manage.php
    - header.php
    - footer.php
    - style_1.css
    
    Changes:
    
    - index.html -> index.php
    
    Notes:
    
    - header.php and footer.php -> Section 1 (Reusable common element PHP)
    - manage.php is now can display data from MySQL to a table on the webpage
    - navbar update with new link for php page
    
    Things need to work on:
    
    - manage.php -> add form to a table in order for manager to be able to change a status of the application
    - manage.php -> Add search function for the manager to be able to search for specific application either by applicant name, job position, skills or even job description(Sorting)
    - CSS still need to be fix
    - Hamburger menu not done
    - Further responsive design (Text size, space)

---
+++ manage.php
@@ -0,0 +1,131 @@
+<?php include("./header.php"); ?>
+
+<title>Management</title>
+</head>
+
+<body>
+
+
+
+	<!-- Navbar -->
+	<nav id="main-nav" class="container">
+
+
+		<h1 id="logo">Five-5</h1>
+		<!-- Hamburger Menu -->
+		<label class="hamburger_menu">
+			<input type="checkbox">
+		</label>
+		<aside class="sidebar">
+
+			<!-- <div><a href="index.html" class="active">Home</a></div>
+			<div><a href="about.html">About Us</a></div>
+			<div><a href="jobs.html">Jobs Description</a></div>
+			<div><a href="apply.html">Apply Now</a></div>
+			<div><a href="enhancement.html">Our Website Enhancement</a></div> -->
+
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="about.html">About Us</a></li>
+				<li><a href="jobs.html">Jobs Description</a></li>
+				<li><a href="apply.html">Apply Now</a></li>
+				<li><a href="manage.php" class="active">Management</a></li>
+				<li><a href="enhancement.html">Our Website Enhancement</a></li>
+			</ul>
+		</aside>
+
+
+	</nav>
+
+	<?php
+
+
+	include('settings.php');
+	// Variable
+	// $Firstname = $_POST['Firstname'];
+	// $Lastname = $_POST['Lastname'];
+	// $Dob = $_POST['Dob'];
+	// $Gender = $_POST['Gender'];
+	// $Email = $_POST['Email'];
+	// $Phone = $_POST['Phone'];
+	// $Address = $_POST['Address'];
+	// $Suburb = $_POST['Suburb'];
+	// $State = $_POST['State'];
+	// $Postcode = $_POST['Postcode'];
+	// $Job_prefer = $_POST['Job_prefer'];
+	// $Job_reference_number = $_POST['Job_reference_number'];
+	// $Programming_language = $_POST['Programming_Language'];
+	// $Skills = $_POST['Skills'];
+	
+
+
+	$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+	if (!$conn) {
+		echo "<p>Database connection failure</p>";
+	} else {
+
+		$sql_table = "ApplyForm_Assignment2";
+		$query = "SELECT * FROM $sql_table ORDER BY ID DESC";
+		$result = mysqli_query($conn, $query);
+
+		if (!$result) {
+			echo "<p>Something is wrong with" . $query . "</p>";
+		} else {
+			echo "<table>\n";
+			echo "<tr>\n"
+				. "<th scope=\"col\">Application ID</th>\n"
+				. "<th scope=\"col\">Job Position</th>\n"
+				. "<th scope=\"col\">Job Reference No.</th>\n"
+				. "<th scope=\"col\">Firstname</th>\n"
+				. "<th scope=\"col\">Lastname</th>\n"
+				. "<th scope=\"col\">Gender</th>\n"
+				. "<th scope=\"col\">DOB</th>\n"
+				. "<th scope=\"col\">Programming Language</th>\n"
+				. "<th scope=\"col\">Skills</th>\n"
+				. "<th scope=\"col\">Email</th>\n"
+				. "<th scope=\"col\">Phone</th>\n"
+				. "<th scope=\"col\">Address</th>\n"
+				. "<th scope=\"col\">Suburb</th>\n"
+				. "<th scope=\"col\">State</th>\n"
+				. "<th scope=\"col\">Postcode</th>\n"
+				. "<th scope=\"col\">Status</th>\n"
+				. "<tr>\n";
+
+			while ($row = mysqli_fetch_assoc($result)) {
+				echo "<tr>\n";
+				echo "<td>", $row["ID"], "</td>";
+				echo "<td>", $row["Job_prefer"], "</td>";
+				echo "<td>", $row["Job_reference_number"], "</td>";
+				echo "<td>", $row["Firstname"], "</td>";
+				echo "<td>", $row["Lastname"], "</td>";
+				echo "<td>", $row["Gender"], "</td>";
+				echo "<td>", $row["Dob"], "</td>";
+				echo "<td>", $row["Programming_Language"], "</td>";
+				echo "<td>", $row["Skills"], "</td>";
+				echo "<td>", $row["Email"], "</td>";
+				echo "<td>", $row["Phone"], "</td>";
+				echo "<td>", $row["Address"], "</td>";
+				echo "<td>", $row["Suburb"], "</td>";
+				echo "<td>", $row["State"], "</td>";
+				echo "<td>", $row["Postcode"], "</td>";
+				if (!$row["Status"]) {
+					echo "<td>Waiting</td>";
+				} else {
+					echo "<td>", $row["Postcode"], "</td>";
+				}
+				echo "<tr>\n";
+			}
+			echo "</table>\n";
+			mysqli_free_result($result);
+		}
+		mysqli_close($conn);
+
+	}
+
+
+
+
+
+	include("./footer.php");
+	?>
