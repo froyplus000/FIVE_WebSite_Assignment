b6e54609df1b812470866c563419838fa05f0e8d
HuyNgocDuong <ngochuyduong288@gmail.com>
2023-10-22 20:04:25 +0700

    Update processEOI.php

--- processEOI.php
+++ processEOI.php
@@ -69,26 +69,95 @@ if(mysqli_connect_errno()){
     mysqli_stmt_bind_param($stmt, "ssssssssssssss", $Firstname, $Lastname, $Dob, $Gender, $Email, $Phone, $Address, $Suburb, $State, $Postcode, $Job_prefer, $Job_reference_number, $Programming_language, $Skills);
     mysqli_stmt_execute($stmt);
 // Generate a ID
-$query = "SELECT * FROM ApplyForm_Assignment2 ORDER BY ID ASC";
-$result = mysqli_query($con,$query);
-if (!$result) {
-    echo "Query failed: " . mysqli_error($con);
-} else {
-    // Initialize $id before the while loop
-    $id = null;
+// $query = "SELECT * FROM ApplyForm_Assignment2 ORDER BY ID ASC";
+// $result = mysqli_query($con,$query);
+// if (!$result) {
+//     echo "Query failed: " . mysqli_error($con);
+// } else {
+//     // Initialize $id before the while loop
+//     $id = null;
     
-    // Fetch the data
-    while ($row = mysqli_fetch_assoc($result)) {
-        $id = $row["ID"];
-    }
+//     // Fetch the data
+//     while ($row = mysqli_fetch_assoc($result)) {
+//         $id = $row["ID"];
+//     }
 
-    // Close the database connection
-    mysqli_close($con);
-    //Print out the message
-    if ($id !== null) {
-        echo "<p class='mycss1'>Sending successfully <br> Your Application ID is: $id</br>After receive the Application ID, please wait until there is a email send to you to let you know you are hire or not. </p>";
-    } else {
-        echo "No ID found in the database.";
+//     // Close the database connection
+//     mysqli_close($con);
+//     //Print out the message
+//     if ($id !== null) {
+//         echo "<p class='mycss1'>Sending successfully <br> Your Application ID is: $id</br>After receive the Application ID, please wait until there is a email send to you to let you know you are hire or not. </p>";
+//     } else {
+//         echo "No ID found in the database.";
+//     }
+// }
+if(!$con){
+    echo "<p>Information from the application form<\p>";
+}else{
+    $query = "SELECT * FROM ApplyForm_Assignment2 WHERE ID ASC";
+    $result = mysqli_query($con,$query);
+    
+    if(!$result){
+        echo "<p>Something is wrong with" . $query . "</p>";
+    }else{
+        echo "<form class=\"outter-wrapper\" action=\"status_update.php\" method=\"post\">";
+			echo "<div class=\"outter-wrapper\">";
+			echo "<div class=\"table-wrapper\">";
+			echo "<table id=\"m_table\">\n";
+			echo "<tr>\n"
+				. "<th scope=\"col\">Application ID</th>\n"
+				. "<th scope=\"col\">Status</th>\n"
+				. "<th scope=\"col\">Job Position</th>\n"
+				. "<th scope=\"col\">Job Reference No.</th>\n"
+				. "<th scope=\"col\">Firstname</th>\n"
+				. "<th scope=\"col\">Lastname</th>\n"
+				. "<th scope=\"col\">Gender</th>\n"
+				. "<th scope=\"col\">DOB</th>\n"
+				. "<th scope=\"col\">Programming Language</th>\n"
+				. "<th scope=\"col\">Skills</th>\n"
+				. "<th scope=\"col\">Email</th>\n"
+				. "<th scope=\"col\">Phone</th>\n"
+				. "<th scope=\"col\">Address</th>\n"
+				. "<th scope=\"col\">Suburb</th>\n"
+				. "<th scope=\"col\">State</th>\n"
+				. "<th scope=\"col\">Postcode</th>\n"
+				. "<th scope=\"col\">Accept</th>\n"
+				. "<th scope=\"col\">Decline</th>\n"
+				. "<th scope=\"col\">Submit</th>\n"
+				. "<tr>\n";
+            
+            while($row = mysqli_fetch_assoc($result)){
+                echo "<tr>\n";
+				echo "<td>", $row["ID"], "</td>";
+
+				// echo "<td>", $row["Status"], "</td>";
+				if (!$row["Status"]) {
+					echo "<td>Pending</td>";
+				} else {
+					echo "<td>", $row["Status"], "</td>";
+				}
+
+				echo "<td>", $row["Job_prefer"], "</td>";
+				echo "<td>", $row["Job_reference_number"], "</td>";
+				echo "<td>", $row["Firstname"], "</td>";
+				echo "<td>", $row["Lastname"], "</td>";
+				echo "<td>", $row["Gender"], "</td>";
+				echo "<td>", $row["Dob"], "</td>";
+				echo "<td>", $row["Programming_Language"], "</td>";
+				echo "<td>", $row["Skills"], "</td>";
+				echo "<td>", $row["Email"], "</td>";
+				echo "<td>", $row["Phone"], "</td>";
+				echo "<td>", $row["Address"], "</td>";
+				echo "<td>", $row["Suburb"], "</td>";
+				echo "<td>", $row["State"], "</td>";
+				echo "<td>", $row["Postcode"], "</td>";
+            }
+            echo "</table>\n";
+			echo "</div>\n";
+			echo "</div>\n";
+			echo "</div>\n";
+			mysqli_free_result($result);
     }
+    mysqli_close($conn);
 }
 ?>
